2522d49fdd2282e0f11113422782577a88714645
Richard Melchior <raustin.melchior@gmail.com>
2018-05-24 20:22:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,7 @@ function init() {
 
   let index = 0;
 
-  const body = document.getElementsByTagName('body');
+  const body = document.querySelector('body');
 
   body.addEventListener('keydown',
     function onKeyDownHandler(e){
